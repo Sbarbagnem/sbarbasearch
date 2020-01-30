9e68364dcce9bd8ec7e2903561cc4693d64121c1
belerico <belo.fede@outlook.com>
2020-01-30 23:34:35 +0100

    Add preprocess as function

--- compass_embeddings/create_twec_data.py
+++ compass_embeddings/create_twec_data.py
@@ -15,24 +15,31 @@ if __name__ == "__main__":
         bigram = Phraser.load(os.path.join("data", "models", "bigram.pkl"))
         trigram = Phraser.load(os.path.join("data", "models", "trigram.pkl"))
 
+    print("* LOADING QUERY SENTENCES")
     sentences = pickle.load(open(os.path.join("data", "query", "tweet.pkl"), "rb"))
     if compute_n_grams:
+        print("* COMPUTING BIGRAMS/TRIGRAMS")
         sentences = trigram[bigram[sentences]]
     sentences = " ".join(list(itertools.chain(*sentences)))
     with open(os.path.join("data", "twec", "tweets.txt"), "w+") as f:
+        print("* SAVING QUERY TXT")
         f.write(sentences)
 
     for user in USERS_LIST:
+        print("* LOADING " + user + " SENTENCES")
         user_sentences = pickle.load(
             open(os.path.join("data", "users", user + ".pkl"), "rb")
         )
         if compute_n_grams:
+            print("* COMPUTING BIGRAMS/TRIGRAMS")
             user_sentences = trigram[bigram[user_sentences]]
         user_sentences = " ".join(list(itertools.chain(*user_sentences)))
         sentences = sentences + " " + user_sentences
         with open(os.path.join("data", "twec", user + ".txt"), "w+") as f:
+            print("* SAVING " + user + " TXT")
             f.write(user_sentences)
 
     with open(os.path.join("data", "twec", "compass.txt"), "w+") as f:
+        print("* SAVING COMPASS TXT")
         f.write(sentences)
 
--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -11,31 +11,22 @@ from pandarallel import pandarallel
 from preprocess.tweet_preprocess import TweetPreprocess
 
 
-def save(data, filenames):
-    print("* SAVING")
-    for i, d in enumerate(data[0]):
-        with open(filenames[i], "wb+") as f:
-            pickle.dump(d, f, protocol=pickle.HIGHEST_PROTOCOL)
-            f.flush()
-            f.close()
-
-
-if __name__ == "__main__":
+def preprocess(return_preprocessed=False):
     pandarallel.initialize(nb_workers=mp.cpu_count() - 1, progress_bar=True)
-    length = [0]
+    lengths = [0]
     load_filenames = [os.path.join("data", "query", "tweet.json")] + [
         os.path.join("data", "users", user + ".json") for user in USERS_LIST
     ]
     save_filenames = [os.path.join("data", "query", "tweet.pkl")] + [
         os.path.join("data", "users", user + ".pkl") for user in USERS_LIST
     ]
-    print('* LOADING TWEETS')
+    print("* LOADING TWEETS")
     tweets = pandas.read_json(
         load_filenames[0], orient="columns", convert_axes=False, convert_dates=False,
     )[["text"]]
-    length.append(len(tweets))
+    lengths.append(len(tweets))
     for i in range(1, len(load_filenames)):
-        print('* LOADING ' + USERS_LIST[i-1] + ' TWEETS')
+        print("* LOADING " + USERS_LIST[i - 1] + " TWEETS")
         users = (
             pandas.read_json(
                 load_filenames[i],
@@ -46,23 +37,31 @@ if __name__ == "__main__":
             .reset_index(drop=True)
             .rename(columns={0: "text"})
         )
-        length.append(len(users))
+        lengths.append(len(users))
         tweets = pandas.concat([tweets, users], ignore_index=True)
-    print('* DONE')
-    length = np.cumsum(length)
+    lengths = np.cumsum(lengths)
     init = time.time()
-    print('* PREPROCESSING')
-    tweets['text'] = tweets.parallel_apply(
+    print("* PREPROCESSING")
+    tweets["text"] = tweets.parallel_apply(
         lambda x: TweetPreprocess.preprocess(x.text, return_list=False), axis=1
     )
-    print('* DONE')
-    print('* ELAPSED TIME: {:.4f}'.format(time.time() - init))
-    for i in range(1, len(length)):
-        print('* SAVING PREPROCESSED TWEETS TO ' + save_filenames[i - 1])
-        chunk = tweets.iloc[length[i - 1] : length[i], :]
+    print("* ELAPSED TIME: {:.4f}".format(time.time() - init))
+    if return_preprocessed:
+        preprocessed = [0] * len(load_filenames)
+    for i in range(1, len(lengths)):
+        print("* SAVING PREPROCESSED TWEETS TO " + save_filenames[i - 1])
+        chunk = tweets.iloc[lengths[i - 1] : lengths[i], :]
         values = chunk.text.apply(str.split).values.tolist()
+        if return_preprocessed:
+            preprocessed[i - 1] = values
         with open(save_filenames[i - 1], "wb+") as f:
             pickle.dump(values, f, protocol=pickle.HIGHEST_PROTOCOL)
             f.flush()
             f.close()
+    if return_preprocessed:
+        return preprocessed
+
+
+if __name__ == "__main__":
+    preprocess(return_preprocessed=False)
 
